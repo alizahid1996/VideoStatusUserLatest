3566392ee414b3dd54c468873e0f7c6c138fb041
zia1122 <ziarehman.cs@gmail.com>
2021-04-13 00:17:41 +0500

    commit 3 Zia

--- app/src/main/java/com/example/videostatususerlatest/MainActivity.java
+++ app/src/main/java/com/example/videostatususerlatest/MainActivity.java
@@ -6,12 +6,14 @@ import androidx.appcompat.widget.SearchView;
 import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -20,7 +22,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.videostatususerlatest.Adapters.VideoPlayerRecyclerAdapter;
+import com.example.videostatususerlatest.Adapters.VideoPlayerRecyclerView;
+import com.example.videostatususerlatest.Models.MediaObject;
 import com.example.videostatususerlatest.Models.Member;
+import com.example.videostatususerlatest.utils.VerticalSpacingItemDecorator;
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.exoplayer2.SimpleExoPlayer;
@@ -34,292 +44,50 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
-    DatabaseReference databaseReference;
-    RecyclerView recyclerView;
-    ViewHolder viewHolder;
-    FirebaseDatabase database;
-    public SimpleExoPlayer exoPlayer;
-    String name,url;
-    PlayerView playerView;
-    View view;
-    private ProgressDialog progressDialog;
-    FirebaseUser currentUser;//used to store current user of account
-    FirebaseAuth mAuth;//Used for firebase authentication
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private VideoPlayerRecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      //  recyclerView = findViewById(R.id.recyclerview_ShowVideo);
-        // recyclerView.setHasFixedSize(true);
-        //recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        database = FirebaseDatabase.getInstance();
-        databaseReference = database.getReference("video");
-        //ShowDialog(this);
 
-        viewHolder = new ViewHolder(view);
+        mRecyclerView = findViewById(R.id.recycler_view);
 
+        initRecyclerView();
+    }
+    private void initRecyclerView(){
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
+        mRecyclerView.addItemDecoration(itemDecorator);
 
+        ArrayList<MediaObject> mediaObjects = new ArrayList<MediaObject>();
+        mRecyclerView.setMediaObjects(mediaObjects);
+        VideoPlayerRecyclerAdapter adapter = new VideoPlayerRecyclerAdapter(mediaObjects, initGlide());
+        mRecyclerView.setAdapter(adapter);
     }
 
-    private void firebaseSearch(String searchtext){
-        ShowDialog(this);
-        String query = searchtext.toLowerCase();
-        Query firebaseQuery = databaseReference.orderByChild("search").startAt(query).endAt(query + "\uf8ff");
+    private RequestManager initGlide(){
+        RequestOptions options = new RequestOptions()
+                .placeholder(R.drawable.white_background)
+                .error(R.drawable.white_background);
 
-        FirebaseRecyclerOptions<Member> options =
-                new FirebaseRecyclerOptions.Builder<Member>()
-                        .setQuery(firebaseQuery, Member.class)
-                        .build();
-
-        FirebaseRecyclerAdapter<Member,ViewHolder> firebaseRecyclerAdapter =
-                new FirebaseRecyclerAdapter<Member, ViewHolder>(options) {
-                    @Override
-                    protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Member model) {
-
-                        holder.setExoplayer(getApplication(),model.getName(),model.getVideourl());
-                        DismissDialog();
-
-                        holder.setOnClicklistener(new ViewHolder.Clicklistener() {
-                            @Override
-                            public void onItemClick(View view, int position) {
-                                name = getItem(position).getName();
-                                url = getItem(position).getVideourl();
-                                Intent intent = new Intent(MainActivity.this,FullScreen.class);
-                                intent.putExtra("nam",name);
-                                intent.putExtra("ur",url);
-                                startActivity(intent);
-
-
-                            }
-
-                            @Override
-                            public void onItemLongClick(View view, int position) {
-
-                                name = getItem(position).getName();
-                                showDeleteDialog(name);
-                            }
-                        });
-                    }
-
-                    @NonNull
-                    @Override
-                    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                        View view = LayoutInflater.from(parent.getContext())
-                                .inflate(R.layout.item,parent,false);
-
-                        return new ViewHolder(view);
-
-                    }
-                };
-
-        firebaseRecyclerAdapter.startListening();
-        recyclerView.setAdapter(firebaseRecyclerAdapter);
+        return Glide.with(this)
+                .setDefaultRequestOptions(options);
     }
+
 
     @Override
-    protected void onStart() {
-        super.onStart();
-
-        ShowDialog(this);
-        FirebaseRecyclerOptions<Member> options =
-                new FirebaseRecyclerOptions.Builder<Member>()
-                        .setQuery(databaseReference,Member.class)
-                        .build();
-
-        FirebaseRecyclerAdapter<Member,ViewHolder> firebaseRecyclerAdapter =
-                new FirebaseRecyclerAdapter<Member, ViewHolder>(options) {
-                    @Override
-                    protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Member model) {
-
-                        holder.setExoplayer(getApplication(),model.getName(),model.getVideourl());
-                        DismissDialog();
-
-                        holder.setOnClicklistener(new ViewHolder.Clicklistener() {
-                            @Override
-                            public void onItemClick(View view, int position) {
-                                name = getItem(position).getName();
-                                url = getItem(position).getVideourl();
-                                Intent intent = new Intent(MainActivity.this,FullScreen.class);
-                                intent.putExtra("nam",name);
-                                intent.putExtra("ur",url);
-                                startActivity(intent);
-
-                            }
-
-                            @Override
-                            public void onItemLongClick(View view, int position) {
-
-                                name = getItem(position).getName();
-                                showDeleteDialog(name);
-                            }
-                        });
-                    }
-
-                    @NonNull
-                    @Override
-                    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                        View view = LayoutInflater.from(parent.getContext())
-                                .inflate(R.layout.item,parent,false);
-
-                        return new ViewHolder(view);
-
-                    }
-                };
-
-        firebaseRecyclerAdapter.startListening();
-        recyclerView.setAdapter(firebaseRecyclerAdapter);
+    protected void onDestroy() {
+        if(mRecyclerView!=null)
+            mRecyclerView.releasePlayer();
+        super.onDestroy();
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.options_menu,menu);
-        MenuItem item = menu.findItem(R.id.search_firebase);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                firebaseSearch(query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                firebaseSearch(newText);
-                return false;
-            }
-        });
-        return super.onCreateOptionsMenu(menu);
-    }
-
-
-    private void showDeleteDialog(String name){
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("Delete");
-        builder.setMessage("Are you Sure to Delete this data");
-        builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-                Query query = databaseReference.orderByChild("name").equalTo(name);
-                query.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                        for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
-                            dataSnapshot1.getRef().removeValue();
-                        }
-                        Toast.makeText(MainActivity.this, "Video Deleted", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        ///
-                    }
-                });
-
-            }
-        });
-
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-                dialogInterface.dismiss();
-            }
-        });
-
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
-
-
-
-    /*
-       To handle the click of option menu items
-    */
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.aboutItem:
-                aboutItemClicked();
-                break;
-            case R.id.feedbackItem:
-                feedbackItemClicked();
-                break;
-            case R.id.uploadmovieItem:
-                updatemovieClicked();
-                break;
-            case R.id.logoutItem:
-                logoutItemClick();
-                break;
-            case R.id.closeItem:
-                closeApplication();
-                break;
-        }
-        return true;
-    }
-    /*
-        Closes the enitre application
-     */
-    private void closeApplication() {
-        finishAffinity();
-        System.exit(0);
-    }
-
-    /*
-        To Logout from the application and not Close.
-     */
-    private void logoutItemClick() {
-        mAuth = FirebaseAuth.getInstance();
-        currentUser = mAuth.getCurrentUser();
-        FirebaseAuth.getInstance().signOut();
-        Toast.makeText(this, "Logged out successfully", Toast.LENGTH_SHORT).show();
-        sendToLoginActivity();
-    }
-    /*
-        To send user to the login page.
-     */
-    private void sendToLoginActivity() {
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-    }
-    /*
-        Send user to the feedback page.
-     */
-    private void feedbackItemClicked() {
-        Intent intent = new Intent(MainActivity.this, FeedbackActivity.class);
-        startActivity(intent);
-    }
-    /*
-        Show the team details to the user.
-     */
-    private void aboutItemClicked() {
-        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
-        startActivity(intent);
-
-    }
-    private void updatemovieClicked() {
-        Intent intent = new Intent(MainActivity.this, UplaodMovie.class);
-        startActivity(intent);
-
-    }
-
-    public void ShowDialog(Context context) {
-        //setting up progress dialog
-        progressDialog = new ProgressDialog(context);
-        progressDialog.show();
-        progressDialog.setContentView(R.layout.progress_dialog);
-        progressDialog.setCanceledOnTouchOutside(false);
-        progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-    }
-    public void DismissDialog() {
-        progressDialog.dismiss();
-    }
-
-
 }
